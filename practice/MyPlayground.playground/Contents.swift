1070c6b8169d50e7aaab3693609d2fd81a232334
leetaek <retake_joy@naver.com>
2022-01-05 16:21:36 +0900

    [Study] extension의 개념과 활용

--- practice/MyPlayground.playground/Contents.swift
+++ practice/MyPlayground.playground/Contents.swift
@@ -1,37 +1,32 @@
 import UIKit
 
-// Structure
-// 구조체
-// class는 reference type이지만 sturct는 value type.
-// sturct는 상속이 안됨.
-struct ImageType {
-    var type = ""
+// extension
+//  기능 확장
+// struct, class, enum, protocol 등에 사용 가능
+// ex. 숫자(int) 짝수, 홀수인지에 따라 여러 구조에서 실행되야 함
+extension Int {                 // Int라는 것에 기능 추가
+    var oddOrEven: String{
+        if self % 2 == 0 {
+            return "짝수"
+        }
+        return "홀수"
+    }
+}
+3.oddOrEven
+4.oddOrEven
+
+// UI의 color를 여러번 사용할 때에 사용.
+// mainColor1 = xxx
+// mainColor2 = xxx
+extension UIColor {
+    class var mainColor1: UIColor {         // 클래스 변수. 이를 참조할때 굳이 인스턴스화 안해도 된다.
+        UIColor(red: 120/255, green: 70/255, blue: 120/255, alpha: 1)
+    }
 }
 
-var imageType1 = ImageType()
-var imageType2 = imageType1
-var imageType3 = imageType2
+var button = UIButton()
+ 
+button.titleLabel?.textColor = .mainColor1         // calss var로 선언했기 때문에 참조할때 클래스 인스턴스 안시켜도 됨.
+// command + control 을 누르고 인스턴스를 클릭하면 참조하는 해당 파일을 확인할 수 있다.
 
-imageType1.type = "jpg"
-imageType2.type = "png"
 
-imageType1.type
-imageType2.type
-imageType3.type
-
-class MyInfo {
-    var myAge = 0
-    
-}
-var info1 = MyInfo()
-var info2 = info1
-var info3 = info2
-
-// class는 reference type.
-// 하나의 값을 가지고 참조하는 형태
-info1.myAge = 20
-info2.myAge = 10
-
-info1.myAge
-info2.myAge
-info3.myAge
