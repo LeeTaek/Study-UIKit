f57f8036fdc46f2ffcf4611058ebd5d0bb49f401
leetaek <retake_joy@naver.com>
2022-01-05 20:56:27 +0900

    [Study] generic 의 개념과 where을 사용한 타입 제한

--- practice/MyPlayground.playground/Contents.swift
+++ practice/MyPlayground.playground/Contents.swift
@@ -1,31 +1,38 @@
 import UIKit
 
+// generic <Type 내가 정한 임의의 타입>
+// logic을 반복해서 쓰고 싶은 경우에는 func을 이용.
+// type을 여러가지를 반복해서 쓰고 싶은 경우 generic을 이용
 
+// stack
+struct MyStack<MyType> where MyType: Numeric {                // type은 임의로 아무거나 써도 됨.
+    // where의 타입을 제한할 수 있음. 숫자의 경우 Numeric, 문자의 경우 StringProtocol
+    // where의 경우 내가 만든 타입을 그 스타일 안에서만 사용하고 싶은 경우사용함.
 
-class UserInfo {
-    var name = ""
-    var age = 0
-    func isAdult() -> Bool {                    // protocol과 다르게 class는 기능을 다 구현해두어야 한다.
-        if age > 19 {
-            return true
+    var items = [MyType]()
+    
+    mutating func push(item: MyType) {              // struct 안에서 스스로 변하고 싶은 경우 self가 mutating 사용
+        items.append(item)
+    }
+    
+    mutating func pop() -> MyType? {
+        if items.isEmpty {
+            return nil
         }
-        return false
+        return items.removeLast()
     }
 }
 
-class Guest: UserInfo {
-    override func isAdult() -> Bool {                   // 상속한 class의 기능을 guset class에서는 이걸로 작동
-        return true
-    }
-    
-    func present() {
-        name = "kim"
-        print(name)                                 // override가 없으니 super에 있는 걸 그대로 사용
-        print(super.name)                           // super를 통해 부모 class의 변수에 접근할 수 있음
-        print(super.isAdult())
-        print(isAdult())
-    }
+var myStack = MyStack<String>()
 
+myStack.push(item: "4")
 
-let guest = Guest()
-guest.present()
+var myStack2 = MyStack<Int>()
+myStack2.push(item: 5)
+myStack2.push(item: 6)
+
+myStack2.pop()
+myStack2.pop()
+myStack.pop()
+
+// queue
